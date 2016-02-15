65435d65ca50c0ee4e831442d66546b292ba1cf8
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-02-15 17:52:50 +0000

    TDD FizzBuzz exercise refactor 1

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -3,32 +3,34 @@
         expect( FizzBuzz ).toBeDefined();
       });
 
+    it("should not accept any parameter", function() {
+      expect( FizzBuzz.length ).toEqual(0);
+    });
+
     it("should return a string", function() {
-      var result = FizzBuzz();
-      expect( typeof(result) ).toEqual("string");
+      expect( typeof( FizzBuzz() ) ).toEqual("string");
     });
 
     it("should return 100 lines", function() {
-      var result = FizzBuzz().split('\n');
-      expect( result.length ).toEqual(100);
+      expect( FizzBuzz().split('\n').length ).toEqual( 100 );
     });
 
-    it("should return 'FizzBuzz' for 90", function () {
+    it("should return 'FizzBuzz' for #90", function () {
       var result = FizzBuzz().split('\n')[90-1];
       expect( result ).toEqual('FizzBuzz');
     });
 
-    it("should return 'Fizz' for 33", function () {
+    it("should return 'Fizz' for #33", function () {
       var result = FizzBuzz().split('\n')[33-1];
       expect( result ).toEqual('Fizz');
     });
 
-    it("should return 'Buzz' for 100", function () {
+    it("should return 'Buzz' for #100", function () {
       var result = FizzBuzz().split('\n')[100-1];
       expect( result ).toEqual('Buzz');
     });
 
-    it("should return '97' for 97", function () {
+    it("should return '97' for #97", function () {
       var result = FizzBuzz().split('\n')[97-1];
       expect( result ).toEqual('97');
     });
