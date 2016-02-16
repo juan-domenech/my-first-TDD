0f3a599af1f1a2917094d7532c04b5d363cb2e99
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-02-16 09:03:19 +0000

    TDD rockPaperScissors Some comments

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,6 +1,21 @@
+//Kata: Rock Paper Scissors!
+//Problem Description
+//We need a function rockPaperScissors() that can receive two parameters with the move of each of the two players in the game "Rock Paper Scissors".
+//The possible values are : "PAPER", "SCISSORS" or "ROCK"
+//If the function is called with only one parameter the move of the second player should be randomly generated.
+//>> rockPaperScissors("PAPER","SCISSORS")
+//"PAPER vs SCISSORS => SCISSORS wins!"
+//>> rockPaperScissors("ROCK","ROCK")
+//"ROCK vs ROCK => tie!"
+//>> rockPaperScissors("PAPER")
+//"PAPER vs ROCK => PAPER wins!"
+//>> rockPaperScissors("PAPER")
+//"PAPER vs PAPER => tie!"
+
+
 function rockPaperScissors( player1, player2 ) {
 
-	// We exit with error with no parameters
+	// We exit with error when received no parameters
 	if ( player1 == undefined ){
 		return 'ERROR!';
 	}
@@ -36,6 +51,8 @@ function rockPaperScissors( player1, player2 ) {
 		return player1+' vs '+player2+' => tie!';
 	}
 
+	// Possible outcomes
+
 	if ( ( player1 == 'PAPER') && ( player2 == 'SCISSORS') ) {
 		return 'PAPER vs SCISSORS => SCISSORS wins!';
 	}
@@ -59,7 +76,6 @@ if ( ( player1 == 'SCISSORS') && ( player2 == 'PAPER') ) {
 if ( ( player1 == 'SCISSORS') && ( player2 == 'ROCK') ) {
 		return 'SCISSORS vs ROCK => ROCK wins!';
 	}
-
 
 
 }
